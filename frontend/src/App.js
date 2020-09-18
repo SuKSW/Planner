b952dfd252b948f4f132cb0fe6b59e87c1e1a7d6
Subhashinie Koshalya <subhashinieksw.14@cse.mrt.ac.lk>
2020-09-18 12:21:05 +0530

    frontend: add tailswind and its dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="text-2xl font-bold mt-8 mb-5">An h1 tag</h1>
+        <button className="btn btn-purple">
+          A Button
+        </button>
       </header>
     </div>
   );
