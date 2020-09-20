4a08acbaa254b6f970a02c9e1fececb4cb326174
Subhashinie Koshalya <subhashinieksw.14@cse.mrt.ac.lk>
2020-09-20 11:14:23 +0530

    frntend_mui: add routes

--- frontend_mui/src/App.js
+++ frontend_mui/src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import SideNavbar from './components/common/sideNavbar'
+import Today from './components/pages/today'
+import ThisMonth from './components/pages/thisMonth'
+import Todos from './components/pages/todos'
+import Appointments from './components/pages/appointments'
+import Notes from './components/pages/notes'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <SideNavbar/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <SideNavbar>
+                <Switch>
+                    <Route path="/" component={Today} exact/>
+                    <Route path="/this-month" component={ThisMonth} />
+                    <Route path="/todos" component={Todos} />
+                    <Route path="/appointments" component={Appointments} />
+                    <Route path="/notes" component={Notes} />
+                </Switch>
+            </SideNavbar>
+        </div>
+    );
 }
 
 export default App;
---
+++ frontend_mui/src/components/pages/today.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Today() {
+    return (
+        <div>
+            today
+        </div>
+    );
+}
+
+export default Today;
